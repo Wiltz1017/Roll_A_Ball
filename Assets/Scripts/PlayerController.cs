924cb1922f3fd83a9470ac721b7fcc5cc1e8ad58
Wiltz1017 <CTWiltz02@gmail.com>
2016-07-20 15:22:47 -0500

    Initial commit
    first commit to github

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour 
+{	
+	public float speed;
+	public Text countText;
+	public Text WinText;
+
+	private Rigidbody rb;
+	private int count;
+
+	void Start()
+	{
+		rb = GetComponent<Rigidbody> ();
+		count = 0;
+		SetCountText ();
+		WinText.text = "";
+	}
+
+	void FixedUpdate()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal") * speed;
+		float moveVertical = Input.GetAxis ("Vertical") * speed;
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		rb.AddForce (movement);
+	}
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.CompareTag ("pickup") || other.gameObject.CompareTag("Emerald")) 
+		{
+			other.gameObject.SetActive (false);
+			count = count + 1;
+			SetCountText ();
+		}
+	
+	}
+	void SetCountText ()
+	{
+		countText.text = "Count:" + count.ToString ();
+		if (count >= 144)
+		{
+			WinText.text = "You Win"; 
+		}
+	}
+}
---
+++ Assets/Scripts/obsticle.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class obsticle : MonoBehaviour {
+	
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		transform.Rotate (new Vector3 (0, 0, 80) * Time.deltaTime);
+	}
+}
